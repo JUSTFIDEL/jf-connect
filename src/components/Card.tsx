25c01916929f745efbeda42427dbb984310b6597
Fidelis Adang <fidenki1429@gmail.com>
2025-04-05 23:27:17 +0100

    image adjustment

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,9 +10,9 @@ type Props = {
 
 const Card = ({ title, desc, amount, image }: Props) => {
   return (
-    <div className='flex flex-col flex-1 rounded-[10px] bg-[#252539]/80 p-3 min-w-[110] sm:max-w-[180] my-4 '>
+    <div className='flex flex-col flex-1 rounded-[10px] bg-[#252539]/80 p-3 min-w-[100] sm:max-w-[180] my-4 '>
       <div className='w-full bg-gray-400 rounded mb-2'>
-        <Image src={image} alt='image' width={200} height={160} />
+        <Image src={image} alt='image' width={180} height={160} />
       </div>
       <div className='flex flex-col w-full text-left gap-1'>
         <h1 className='text-[12px] font-bold'>{title}</h1>
